ab3edc5ae07e631137f497673a0800133af1f3ed
17323410065 <1172205181@qq.com>
2019-01-08 10:53:38 +0800

    登录

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import controlStyle from './modules/controlStyle'
 import app from './modules/app'
+import getters from './getters'
+import user from './modules/user'
 
 Vue.use(Vuex);
 
@@ -9,7 +11,9 @@ Vue.use(Vuex);
 export default new Vuex.Store({
     modules: {
         controlStyle,
+        user,
         app
     },
+    getters
 });
 
