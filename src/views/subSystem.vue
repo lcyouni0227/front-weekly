8938e048fb2ea2a8e1f3481ff88ad75e46621e2a
xiong <465898137@qq.com>
2019-01-26 22:23:00 +0800

    update

--- src/views/subSystem.vue
+++ src/views/subSystem.vue
@@ -3,13 +3,13 @@
         <el-header style="height: auto">
             <el-form :inline="true" class="demo-form-inline">
                 <el-form-item label="审批人">
-                    <el-input placeholder="审批人"></el-input>
+                    <el-input v-bind="query.date1" placeholder="审批人"></el-input>
                 </el-form-item>
                 <el-form-item label="审批人">
-                    <el-input placeholder="审批人"></el-input>
+                    <el-input v-bind="query.date2" placeholder="审批人"></el-input>
                 </el-form-item>
                 <el-form-item>
-                    <el-button type="primary">查询</el-button>
+                    <el-button @click="$tableRowOperation.query(this)" type="primary">查询</el-button>
                 </el-form-item>
             </el-form>
         </el-header>
@@ -18,23 +18,23 @@
                 <el-table-column type="index" label="序" align="center"></el-table-column>
                 <el-table-column prop="date" label="日期" width="150">
                     <template scope="scope">
-                        <span v-if="handleEdit(scope.$index)">{{scope.row.date}}</span>
+                        <span v-if="!$tableRowOperation.hasEdit(scope)">{{scope.row.date}}</span>
                         <el-input v-else size="small" v-model="scope.row.date" placeholder="请输入内容"></el-input>
                     </template>
                 </el-table-column>
                 <el-table-column prop="name" label="姓名" width="120">
                     <template scope="scope">
-                        <span v-if="!$tableRowOperation.getTableRowIsEdit(scope)">{{scope.row.name}}</span>
+                        <span v-if="!$tableRowOperation.hasEdit(scope)">{{scope.row.name}}</span>
                         <el-input v-else size="small" v-model="scope.row.name" placeholder="请输入内容"></el-input>
                     </template>
                 </el-table-column>
                 <el-table-column label="操作">
                     <template scope="scope">
-                        <el-button v-if="$tableRowOperation.showTableRowAddBtn(scope)" @click="$tableRowOperation.handelTableRowAdd(scope)" type="primary" icon="el-icon-circle-plus-outline" plain class="tableRowButton">新增</el-button>
-                        <el-button v-if="$tableRowOperation.showTableRowEditBtn(scope)" @click="$tableRowOperation.handelTableRowEdit(scope)" type="warning" icon="el-icon-edit" plain class="tableRowButton">修改</el-button>
-                        <el-button v-if="$tableRowOperation.showTableRowSaveBtn(scope)" @click="$tableRowOperation.handelTableRowSave(scope)" type="warning" icon="el-icon-success" plain class="tableRowButton">保存</el-button>
-                        <el-button v-if="$tableRowOperation.showTableRowCancelBtn(scope)" @click="$tableRowOperation.handelTableRowCancel(scope)" type="success" icon="el-icon-error" plain class="tableRowButton">取消</el-button>
-                        <el-button v-if="$tableRowOperation.showTableRowDeleteBtn(scope)" @click="$tableRowOperation.handelTableRowDelete(scope)" type="danger" icon="el-icon-circle-close-outline" plain class="tableRowButton">删除</el-button>
+                        <el-button v-if="$tableRowOperation.hasAddButton(scope)" @click="$tableRowOperation.handelTableRowAdd(scope)" type="primary" icon="el-icon-circle-plus-outline" plain class="tableRowButton">新增</el-button>
+                        <el-button v-if="$tableRowOperation.hasEditButton(scope)" @click="$tableRowOperation.handelTableRowEdit(scope)" type="warning" icon="el-icon-edit" plain class="tableRowButton">修改</el-button>
+                        <el-button v-if="$tableRowOperation.hasSaveButton(scope)" @click="$tableRowOperation.handelTableRowSave(scope)" type="warning" icon="el-icon-success" plain class="tableRowButton">保存</el-button>
+                        <el-button v-if="$tableRowOperation.hasCancelButton(scope)" @click="$tableRowOperation.handelTableRowCancel(scope)" type="success" icon="el-icon-error" plain class="tableRowButton">取消</el-button>
+                        <el-button v-if="$tableRowOperation.hasDelButton(scope)" @click="$tableRowOperation.handelTableRowDelete(scope)" type="danger" icon="el-icon-circle-close-outline" plain class="tableRowButton">删除</el-button>
 
                         <el-button @click="$tableRowOperation.test(scope)" type="danger" icon="el-icon-circle-close-outline" plain class="tableRowButton">test</el-button>
                     </template>
@@ -47,19 +47,12 @@
 <script>
     export default {
         name: 'subsystem',
-        methods: {
-            handleCurrentChange(row, event, column) {
-                console.log(row, event, column, event.currentTarget)
-            },
-            handleEdit(index){
-                return this._select == null || this._select.rowNumber != index;
-            },
-            handleDelete(index, row) {
-                console.log(index, row);
-            }
-        },
         data() {
             return {
+                query:{
+                    date1:'',
+                    date2:''
+                },
                 keyField:"date",
                 module:'xx',
                 rows: [{
