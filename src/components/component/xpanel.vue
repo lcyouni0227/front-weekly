16a66b13f01087830e24d4240e631923aef7df29
xiong <465898137@qq.com>
2019-02-24 23:44:28 +0800

    update

--- src/components/component/xpanel.vue
+++ src/components/component/xpanel.vue
@@ -1,46 +1,128 @@
 <template>
-    <div class="d-panel" :class="{ noheader: !title, expanded: expanded }">
-        <div class="d-panel-header" v-if="title">{{title}}<slot name="header"></slot></div>
-        <div class="d-panel-content" transition="collapse" v-show="expanded">
-            <slot></slot>
+    <div class="collapse collapse-item" :class="{'is-active': active}" :style="{width:width}">
+        <div class="collapse-header touchable" :aria-expanded="active?'true':'false'" v-if="title">
+            {{title}}
+            <i :class="getIcon" class="icon" @click.prevent="toggle"></i>
         </div>
+        <transition name="fade">
+            <div class="collapse-content-box" v-if="active" :style="{height:height}" :class="{scrollclass:scroll}" >
+                <slot></slot>
+            </div>
+        </transition>
     </div>
 </template>
 
-<style>
-    .d-panel {
-        background-color: #ffffff;
-        box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 1px 5px 0 rgba(0, 0, 0, 0.12);
-    }
-    .d-panel-header {
-        padding: 14px 15px 7px;
-        font-size: 14px;
-    }
-    .d-panel-content {
-        box-sizing: border-box;
-        padding: 15px 20px 20px 20px;
-        display: none;
-    }
-    .d-panel.noheader .d-panel-content {
-        border-radius: 5px;
-    }
-    .d-panel.expanded .d-panel-content {
-        display: block;
-    }
-</style>
-
-<script type="text/ecmascript-6">
+<script>
     export default {
-        name:'XPanel',
-        props: {
-            title: {},
-            expanded: {
-                type: Boolean,
-                default: true
+        name: "XPanel",
+        data() {
+            return {
+                active: this.selected
             }
         },
-        transitions: {
-            collapse: require('./support/collapse-transition').default
+        props: {
+            selected: {type: Boolean, default: true},   /* 折叠或展开状态 */
+            title: String,
+            height: String,
+            width: String,
+            scroll:{type: Boolean, default: true}
+        },
+        computed:{
+            getIcon(){
+                return this.active?'el-icon-arrow-up':'el-icon-arrow-down';
+            }
+        },
+        methods: {
+            toggle() {
+                this.active = !this.active;
+            }
         }
-    };
+    }
 </script>
+<style scoped>
+    .collapse {
+        margin: 1px;
+        display: inline-block;
+        overflow-y: hidden;
+    }
+    .icon{
+        float: right;
+        display: inline-block;
+    }
+    .collapse .collapse-header {
+        padding: 15px;
+        background: #f5f7fa;
+        border-radius: 3px;
+        position: relative;
+    }
+
+    .collapse .collapse-header > div {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .collapse .collapse-header h3 {
+        font-size: 0.938em;
+        font-weight: bold;
+    }
+
+    .collapse .collapse-header::before {
+        overflow: scroll;
+        -moz-transition: all .2s;
+        -o-transition: all .2s;
+        -webkit-transition: all .2s;
+        transition: all .2s;
+        position: absolute;
+        font-size: 0.4em;
+        top: calc(50% - 0.4em);
+        left: 20px;
+        color: #c5c9d0;
+        -moz-transform: rotate(-90deg);
+        -o-transform: rotate(-90deg);
+        -ms-transform: rotate(-90deg);
+        -webkit-transform: rotate(-90deg);
+        transform: rotate(-90deg);
+    }
+
+    .collapse.is-active .collapse-header::before {
+        -moz-transform: rotate(0deg);
+        -o-transform: rotate(0deg);
+        -ms-transform: rotate(0deg);
+        -webkit-transform: rotate(0deg);
+        transform: rotate(0deg);
+    }
+
+    .collapse .collapse-content-box {
+        line-height: 22px;
+        -moz-transition: all .2s;
+        -o-transition: all .2s;
+        -webkit-transition: all .2s;
+        transition: all .2s;
+        padding: 15px;
+        border-left: 2px solid #f7f7f7;
+        border-bottom: 2px solid #f7f7f7;
+        border-right: 2px solid #f7f7f7;
+        border-top: 2px solid #f7f7f7;
+        border-bottom-left-radius: 3px;
+        border-bottom-right-radius: 3px;
+    }
+    /*滚动条样式*/
+    .collapse-content-box::-webkit-scrollbar {
+        width: 8px;
+        height: 8px;
+    }
+    .collapse-content-box::-webkit-scrollbar-thumb {/*滚动条里面小方块*/
+        border-radius: 15px;
+        -webkit-box-shadow: inset 0 0 2px rgba(0,0,0,0.1);
+        background: rgba(0,0,0,0.1);
+    }
+    .collapse-content-box::-webkit-scrollbar-track {/*滚动条里面轨道*/
+        -webkit-box-shadow: inset 0 0 2px rgba(0,0,0,0.1);
+        border-radius: 5px;
+        background: rgba(0,0,0,0);
+    }
+    .scrollclass {
+        overflow: scroll;
+    }
+</style>
