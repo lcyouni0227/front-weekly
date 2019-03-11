045fb9f0a8261be9bb4c7cf1bc479b51aed775f4
xiong <465898137@qq.com>
2019-03-11 22:13:57 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,12 @@
         <router-view style="height: 100%"/>
     </div>
 </template>
-
+<style scoped>
+    html, body, #app {
+        width: 100%;
+        height: 100%;
+    }
+</style>
 <style lang="less">
     @import "common/base/common.less";
 </style>
